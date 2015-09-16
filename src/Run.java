c46609c31d279b48809c52dbe7f31ac5dd1d5257
Kenneth Vittetoe <kenv@islandgamestudios.com>
2015-09-16 17:12:40 -0600

    CartaEncapsulada

--- src/Run.java
+++ src/Run.java
@@ -2,37 +2,9 @@
 public class Run {
 	public static void main (String[] args)
 	{
-		int a;
-		Foo f;
-		f = new Foo(5);
-		
-		System.out.println(f);
-		f.setX(10);
-		System.out.println(f.getX());
-		
-		Foo afoo[] = new Foo[f.getX()];
-		
-		for (int i = 0; i < afoo.length; i++) {
-			afoo[i] = new Bar(i+1,afoo.length - i);
-		}
-		
-		
-		//Invertir
-		Foo[] temp = new Foo[afoo.length];
-		int ultimo = afoo.length -1;
-		for(int i =0; i<afoo.length;i++)
-		{
-			temp[ultimo - i] = afoo[i];
-		}
-		
-		afoo = temp;
-		
-
-		for (Foo foo : afoo) {
-			System.out.println(foo);
-		}
-		
+		CartaEncapsulada AsTrebol =
+				new CartaEncapsulada(CartaEncapsulada.NUMERO_AS
+						, CartaEncapsulada.MANJAR_TREBOL);
+		System.out.println(AsTrebol);
 	}
-
-
 }
