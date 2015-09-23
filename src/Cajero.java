36683570213d132b1f7aceb54b23bd666face15c
Kenneth Vittetoe <kenv@islandgamestudios.com>
2015-09-23 17:03:49 -0600

    leer archivo

--- src/Cajero.java
+++ src/Cajero.java
@@ -3,5 +3,9 @@ public class Cajero {
 	public String m_szNombre;
 	public float m_fSalario;
 	public float m_fComision;
-	
+	public String toString()
+	{
+		return m_szNombre +" "+m_fSalario+" "+m_fComision ;
+		
+	}
 }
--- src/Run.java
+++ src/Run.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 public class Run {
 	public static void main (String[] args)
 	{
@@ -8,17 +11,34 @@ public class Run {
 			// texto.
 			// binarios.
 		File f = new File("archivo.txt");
+		ArrayList<Cajero> arreglo = new ArrayList<Cajero>();
 		try {
-			FileWriter fw = new FileWriter(f);
-			fw.write("Kenneth Vittetoe ,7000 ,0.3\n");
-			fw.write("David velasquez ,8000 ,0.3\n");
-			fw.write("fernando Benitez ,5000 ,0.3\n");
-			fw.write("Axel Rose ,54000 ,0.2\n");
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
 			
-			fw.close();
+			//linea a leer del BufferdReader
+			String line = null;
+			//mientras hay lineas que leer. lee linea.
+			while ((line = br.readLine()) != null)
+			{
+				// partiendo por la coma
+				String[] partes = line.split(",");
+				Cajero c = new Cajero();
+				c.m_szNombre = partes[0];
+				c.m_fSalario = Float.parseFloat(partes[1]);
+				c.m_fComision = Float.parseFloat(partes[2]);
+				arreglo.add(c);
+			}
+			// cerramos de forma inversa los objetos creados.
+			br.close();
+			fr.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+		for (Cajero cajero : arreglo) {
+			System.out.println(cajero);
 		}
 	}
 }
